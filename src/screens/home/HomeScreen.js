d6c5ce0561fbb4fd43f7c74a2cb9ab5c743bf232
duy-nirvana <trankhanhduy8599@gmail.com>
2021-03-23 20:57:25 +0700

    Change box function color in HomeScreen

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Image } from 'react-native-elements';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Image, Text } from 'react-native-elements';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -15,28 +15,28 @@ function HomeScreen({ navigation }) {
             />
             <View style={styles.row}>
                 <View
-                    style={styles.box_shadow}
+                    style={[styles.box_shadow, styles.greenBg]}
                 >
                     <TouchableOpacity
                         onPress={() => navigation.navigate('Scan')}
                         style={styles.flex_center}
                     >
-                        <Icon name="ios-scan-sharp" size={50} color="black" />
-                        <Text>
+                        <Icon name="ios-scan-sharp" size={50} color="white" />
+                        <Text h4 style={{color: "white"}}>
                             Scan
                         </Text>
                     </TouchableOpacity>
                 </View>
 
                 <View
-                    style={styles.box_shadow}
+                    style={[styles.box_shadow, styles.yellowBg]}
                 >
                     <TouchableOpacity
                         onPress={() => navigation.navigate('Scan')}
                         style={styles.flex_center}
                     >
-                        <Icon name="ios-qr-code-outline" size={50} color="black" />
-                        <Text>
+                        <Icon name="ios-qr-code-outline" size={50} color="white" />
+                        <Text h4 style={{color: "white"}}>
                             Generate QRCode
                         </Text>
                     </TouchableOpacity>
@@ -45,27 +45,27 @@ function HomeScreen({ navigation }) {
 
             <View style={styles.row}>
                 <View
-                    style={styles.box_shadow}
+                    style={[styles.box_shadow, styles.redBg]}
                 >
                     <TouchableOpacity
                         onPress={() => navigation.navigate('Scan')}
                         style={styles.flex_center}
                     >
-                        <FontAwesome5 name="history" size={50} color="black" />
-                        <Text>
+                        <FontAwesome5 name="history" size={50} color="white" />
+                        <Text h4 style={{color: "white"}}>
                             Generate History
                         </Text>
                     </TouchableOpacity>
                 </View>
                 <View
-                    style={styles.box_shadow}
+                    style={[styles.box_shadow, styles.blueBg]}
                 >
                     <TouchableOpacity
                         onPress={() => navigation.navigate('Scan')}
                         style={styles.flex_center}
                     >
-                        <Icon name="people" size={50} color="black" />
-                        <Text>
+                        <Icon name="people" size={50} color="white" />
+                        <Text h4 style={{color: "white"}}>
                             Attendance History
                         </Text>
                     </TouchableOpacity>
@@ -78,20 +78,21 @@ function HomeScreen({ navigation }) {
 
 const styles = StyleSheet.create({
     row: {
-        flex: .3, 
+        flex: .35, 
         flexDirection: 'row', 
         justifyContent: 'center'
     },
     flex_center: {
         flex: 1, 
-        justifyContent: 'center', 
-        alignItems: 'center',
+        justifyContent: 'space-around', 
+        alignItems: 'flex-start',
     },
     box_shadow: {
-        width: '40%', 
-        margin: 5, 
-        backgroundColor: "white",
-        borderRadius: 15,
+        width: '45%', 
+        margin: 5,
+        marginBottom: 10, 
+        borderRadius: 10,
+        padding: 15,
 
         shadowColor: "#000",
         shadowOffset: {
@@ -106,6 +107,18 @@ const styles = StyleSheet.create({
         width: '100%', 
         height: 140, 
         marginBottom: 10
+    },
+    redBg: {
+        backgroundColor: "#f3425f"
+    },
+    blueBg: {
+        backgroundColor: "#1878f3"
+    },
+    greenBg: {
+        backgroundColor: "#45bd63"
+    },
+    yellowBg: {
+        backgroundColor: "#f7b928"
     }
 })
 
