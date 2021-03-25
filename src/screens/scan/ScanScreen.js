9662d2a060c29a0bd60010d0cfcef13f93f06341
duy-nirvana <trankhanhduy8599@gmail.com>
2021-03-25 21:33:32 +0700

    Fix qrcode scanner

--- src/screens/scan/ScanScreen.js
+++ src/screens/scan/ScanScreen.js
@@ -1,13 +1,91 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { Button, CheckBox } from 'react-native-elements';
+import React, { PureComponent } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
-function ScanScreen({ navigation }) {
+const PendingView = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: 'lightgreen',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <Text>Waiting</Text>
+  </View>
+);
+
+class ScanScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {hasScanned: false};
+  }
+  render() {
+  
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor: "tomato" }}>
-            <Text>Scan screen hehehhe</Text>
-        </View>
+      <View style={styles.container}>
+        <RNCamera
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          onBarCodeRead={this.state.hasScanned ? undefined : (e) => this.handleBarCodeScanned(e)}
+          barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
+        >
+          {({ camera, status }) => {
+            if (status !== 'READY') return <PendingView />;
+            return (
+              <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                <TouchableOpacity onPress={() => this.scanAgain(camera)} style={styles.capture}>
+                  <Text style={{ fontSize: 14 }}> Scan Again </Text>
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+        </RNCamera>
+      </View>
     );
+  }
+
+  scanAgain = function(camera) {
+    // const options = { quality: 0.5, base64: true };
+    // const data = await camera.takePictureAsync(options);
+    // //  eslint-disable-next-line
+    // console.log(data.uri);
+    this.setState({hasScanned: false});
+  };
+
+  handleBarCodeScanned = (e) => {
+    console.log('press', e.data)
+    this.setState({hasScanned: true});
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
 
 export default ScanScreen;
