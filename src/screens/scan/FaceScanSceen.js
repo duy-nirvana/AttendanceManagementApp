9aaafd5abd2827fb17ed4e49d677991cccb91a2a
duy-nirvana <trankhanhduy8599@gmail.com>
2021-06-08 23:10:34 +0700

    Reduce quality capture picture and compare it

--- src/screens/scan/FaceScanSceen.js
+++ src/screens/scan/FaceScanSceen.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState, PureComponent, useRef } from 'react';
 import * as RNFS from 'react-native-fs';
 import { useSelector } from 'react-redux';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import axios from 'axios';
 import { Buffer } from "buffer";
 import Face, { Enum, FaceCaptureResponse, LivenessResponse, MatchFacesResponse, MatchFacesRequest, Image as FaceImage } from '@regulaforensics/react-native-face-api-beta'
+import ImageEditor from "@react-native-community/image-editor";
+import { Dimensions } from 'react-native';
 
 var image1 = new FaceImage()
 var image2 = new FaceImage()
@@ -14,19 +16,30 @@ const FaceScanScreen = () => {
     const isCamera = useRef(null);
     const profileUser = useSelector(state => state.profile.profile);
     const [isLoading, setLoading] = useState('nothing');
-
-    useEffect(() => {
-    }, [])
+    const [takeImage, setTakeImage] = useState('');
 
     const takePicture = async () => {
         if (isCamera) {
-          const options = { quality: 0.5, base64: true };
+          const options = { quality: .8, base64: true, fixOrientation: true, mirrorImage: true, width: 120};
           const data = await isCamera.current.takePictureAsync(options);
+
+          //---------------------
           const base64Image = await RNFS.readFile(data.uri, 'base64');
 
-          image2.bitmap = base64Image;
-          image2.type = Enum.eInputFaceType.ift_Live;
-          console.log({image2})
+          //   console.log({image2})
+
+
+          // crop image
+            // let cropData = {
+            //   offset: {x: 10, y: 10},
+            //   size: {width: 200, height: 200},
+            // };
+
+            // let result = await ImageEditor.cropImage(data.uri, cropData)
+            // let finalData = await RNFS.readFile(result, 'base64');
+            image2.bitmap = base64Image;
+            image2.type = Enum.eInputFaceType.ift_Live;
+            setTakeImage(base64Image)
         }
     };
 
@@ -50,7 +63,7 @@ const FaceScanScreen = () => {
         request.images = [image1, image2]
         Face.matchFaces(JSON.stringify(request), response => {
           response = MatchFacesResponse.fromJson(JSON.parse(response))
-          matchedFaces = response.matchedFaces
+          var matchedFaces = response.matchedFaces
           setLoading(matchedFaces.length > 0 ? ((matchedFaces[0].similarity * 100).toFixed(2) + "%") : "error" )
         }, e => { setLoading(e) })
       }
@@ -83,6 +96,13 @@ const FaceScanScreen = () => {
                 </TouchableOpacity>
 
             </View>
+                <Image
+                    source={{uri: `data:image/png;base64,${takeImage}`}}
+                    style={{
+                        width: 100,
+                        height: 110
+                    }}
+                />
                 <Text>{isLoading}</Text>
 		</View>
     )
